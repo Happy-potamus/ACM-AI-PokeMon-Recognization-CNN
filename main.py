f3c2423212cb297c83aeac9f6a1f1eefdac738dd
Happy-potamus <zhoularry121@gmail.com>
2026-03-01 15:46:04 -0800

    folder reordering

---
+++ main.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Transform: convert images to tensors and normalize
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+
+# Download CIFAR-10 dataset
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainloader = DataLoader(trainset, batch_size=64, shuffle=True)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testloader = DataLoader(testset, batch_size=64, shuffle=False)
+
+classes = trainset.classes
+print("Classes:", classes)
+
+
+# Function to show images
+def imshow(img):
+    img = img / 2 + 0.5  # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+# Get some random training images
+dataiter = iter(trainloader)
+images, labels = next(dataiter)
+
+# Show images
+imshow(torchvision.utils.make_grid(images[:8]))
+print('Labels:', [classes[labels[j]] for j in range(8)])
